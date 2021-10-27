94074d511b5cfab5a8c022ae8031ac44e59183fb
Kristians <vintersk@gmail.com>
2021-10-27 12:59:36 +0300

    Processed route validations

--- data-processing/run-one.sh
+++ data-processing/run-one.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 
 echo "Processing: $1.$2"
-ts-node minifyValidations.ts $1 $2
 
-if [ "$2" == "1" ]
-then
-	ts-node defineTripInterval.ts $1 $2
-fi
+ts-node countRouteValidations.ts $1 $2
 
-ts-node filterTrips.ts $1 $2
-ts-node matchVehicles.ts $1 $2
-ts-node matchValidations.ts $1 $2
+# ts-node minifyValidations.ts $1 $2
+
+# if [ "$2" == "1" ]
+# then
+# 	ts-node defineTripInterval.ts $1 $2
+# fi
+
+# ts-node filterTrips.ts $1 $2
+# ts-node matchVehicles.ts $1 $2
+# ts-node matchValidations.ts $1 $2
 
 rm -rf ./temp/*
